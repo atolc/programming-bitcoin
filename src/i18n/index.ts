20e40b791696f3036184e30d80f70d406fc19df7
Maqsood Ahmed <itxhadi27@gmail.com>
2026-07-17 23:45:29 +0500

    feat: add FR/PT/ZH language support (#3)

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,12 +2,18 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import esUi from "../locales/es/ui.json";
 import enUi from "../locales/en/ui.json";
+import frUi from "../locales/fr/ui.json";
+import ptUi from "../locales/pt/ui.json";
+import zhUi from "../locales/zh/ui.json";
 import { DEFAULT_LOCALE } from "../lib/locale";
 
 void i18n.use(initReactI18next).init({
   resources: {
     es: { ui: esUi },
     en: { ui: enUi },
+    fr: { ui: frUi },
+    pt: { ui: ptUi },
+    zh: { ui: zhUi },
   },
   lng: DEFAULT_LOCALE,
   fallbackLng: DEFAULT_LOCALE,
